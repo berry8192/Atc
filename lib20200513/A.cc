bd729499f59aa18357beb47f0365bc201eef5762
berry8192 <berryrt8192.gmail.com>
2021-09-25 18:20:06 +0900

    lib整理

--- lib20200513/A.cc
+++ lib20200513/A.cc
@@ -10,10 +10,11 @@ using namespace std;
 int imax=2147483647;
 long long int llimax=9223372036854775807;
 
-//int型vectorを出力
-void PV(vector<int> pvv) {
-	rep(i, pvv.size()) cout << pvv[i] SP;
-	cout << endl;
+vector<LLi> Cumulative_sum(vector<int> iv){
+    vector<LLi> rtn(iv.size()+1);
+    rtn[0]=0;
+    for(int i=0;i<iv.size();i++) rtn[i+1]=rtn[i]+iv[i];
+    return rtn;
 }
 
 //LLi型vectorを出力
@@ -24,13 +25,15 @@ void PVL(vector<LLi> pvv) {
 
 int main(){
 
-	int a, b, c;
-	cin>> a >> b >> c;
-	string s;
-	cin>> s;
+	int n;
+	vector<int> v;
 
-	if(s=="ABC") cout<< "Yes" << endl;
-	else cout<< "No" << endl;
+	cin>> n;
+	v.resize(n);
+
+	rep(i, n) cin >> v[i];
+
+	PVL(Cumulative_sum(v));
  
 	return 0;
 }
