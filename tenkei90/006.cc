2344436950b8ba405eb005530f94005183c878c9
berry8192 <berryrt8192.gmail.com>
2021-10-09 20:52:13 +0900

    rename_tenkei

---
+++ tenkei90/006.cc
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define all(v) v.begin(), v.end()
+#define SP << " " 
+#define LLi long long int
+
+using namespace std;
+
+int imax=2147483647;
+long long int llimax=9223372036854775807;
+
+//int型vectorを出力
+void PV(vector<int> pvv) {
+	rep(i, pvv.size()) cout << pvv[i] SP;
+	cout << endl;
+}
+
+//LLi型vectorを出力
+void PVL(vector<LLi> pvv) {
+	rep(i, pvv.size()) cout << pvv[i] SP;
+	cout << endl;
+}
+
+int main(){
+
+	int n, k, pos=0, cnt=0;
+	string s, ans="";
+	vector< vector<int> > v;
+
+	cin>> n >> k;
+	cin>> s;
+	v.resize(26);
+	rep(i, 26) v[i].resize(n+1);
+
+	rep(i, n) {
+		rep(j, 26){
+			v[j][i+1]=v[j][i];
+		}
+		v[int(s[i]-'a')][i+1]++;
+	}
+
+	//rep(i, 26) PV(v[i]);
+
+	rep(i, k){
+		if(n-1-pos==k-cnt){
+			//cout<< "syuryou" <<endl;
+			ans+=s.substr(n-(k-cnt));
+			break;
+		}
+		rep(i, 26){
+			//cout<< char('a'+i) SP << v[i][pos] SP << v[i][n-(k-cnt)+1] <<endl;
+			if(v[i][n-(k-cnt)+1]>v[i][pos]){
+				auto lb = lower_bound(all(v[i]), v[i][pos]+1);
+				int tmp=lb-v[i].begin();
+				//cout<< tmp <<endl;
+				cnt++;
+				ans+='a'+i;
+				pos=tmp;
+				//cout<< s[pos-1] <<endl;
+				break;
+			}
+		}
+	}
+
+	cout<< ans <<endl;
+ 
+	return 0;
+}
---
+++ tenkei90/007.cc
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define all(v) v.begin(), v.end()
+#define SP << " " 
+#define LLi long long int
+
+using namespace std;
+
+int imax=2147483647;
+long long int llimax=9223372036854775807;
+
+//int型vectorを出力
+void PV(vector<int> pvv) {
+	rep(i, pvv.size()) cout << pvv[i] SP;
+	cout << endl;
+}
+
+//LLi型vectorを出力
+void PVL(vector<LLi> pvv) {
+	rep(i, pvv.size()) cout << pvv[i] SP;
+	cout << endl;
+}
+
+int main(){
+
+	int n, q, b, l, r;
+	vector<int> v, ans;
+
+	cin>> n;
+	v.resize(n);
+	rep(i, n) cin >> v[i];
+	sort(all(v));
+	v.push_back(imax);
+	//PV(v);
+	
+	cin>> q;
+	ans.resize(q);
+
+	rep(i, q) {
+		cin>> b;
+		auto lb = lower_bound(all(v), b);
+		l=v[lb-v.begin()-1];
+		r=v[lb-v.begin()];
+		//cout<< l SP << r <<endl;
+		ans[i]=min(abs(b-l), abs(b-r));
+	}
+
+	//PV(ans);
+	rep(i, q) cout<< ans[i] <<endl;
+ 
+	return 0;
+}
