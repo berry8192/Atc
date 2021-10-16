ebb0bdf465803cbce3507f0d46d43e8e6c6e37da
berry8192 <berryrt8192.gmail.com>
2021-10-16 20:55:45 +0900

    tmp更新

--- abctmp/E.cc
+++ abctmp/E.cc
@@ -1,6 +1,8 @@
 #include <bits/stdc++.h>
 
 #define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep3(i, n, m) for (int i = m; i < (int)(n); i++)
+#define repr(i, n) for (int i = n-1; i >= 0; i--)
 #define all(v) v.begin(), v.end()
 #define SP << " " 
 #define LLi long long int
@@ -9,17 +11,23 @@ using namespace std;
 
 int imax=2147483647;
 long long int llimax=9223372036854775807;
+LLi mod=1000000007;
+//LLi mod=998244353;
 
 //int型vectorを出力
-void PV(vector<int> pvv) {
+template <class T> void PV(T pvv) {
 	rep(i, pvv.size()) cout << pvv[i] SP;
 	cout << endl;
 }
 
 //LLi型vectorを出力
-void PVL(vector<LLi> pvv) {
-	rep(i, pvv.size()) cout << pvv[i] SP;
-	cout << endl;
+template <class T>void PVV(T pvv) {
+	rep(i, pvv.size()){
+		rep(j, pvv[i].size()){
+			cout << pvv[i][j] SP;
+		}
+		cout << endl;
+	}
 }
 
 int main(){
@@ -31,6 +39,8 @@ int main(){
 	v.resize(n);
 
 	rep(i, n) cin >> v[i];
+	sort(all(v));
+	PV(v);
 
 	rep(i, n) {
 		if (v[i]) ans++;
