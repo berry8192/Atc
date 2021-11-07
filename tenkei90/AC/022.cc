fdad3d8580aa2dde7261955c68b16f24ef051995
berry8192 <berryrt8192.gmail.com>
2021-11-07 20:51:35 +0900

    022AC

---
+++ tenkei90/AC/022.cc
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep3(i, n, m) for (int i = m; i < (int)(n); i++)
+#define repr(i, n) for (int i = n-1; i >= 0; i--)
+#define all(v) v.begin(), v.end()
+#define SP << " " 
+#define LLi long long int
+
+using namespace std;
+
+int imax=2147483647;
+long long int llimax=9223372036854775807;
+LLi mod=1000000007;
+//LLi mod=998244353;
+
+//int型vectorを出力
+template <class T> void PV(T pvv) {
+	rep(i, pvv.size()) cout << pvv[i] SP;
+	cout << endl;
+}
+
+//LLi型vectorを出力
+template <class T>void PVV(T pvv) {
+	rep(i, pvv.size()){
+		rep(j, pvv[i].size()){
+			cout << pvv[i][j] SP;
+		}
+		cout << endl;
+	}
+}
+
+int main(){
+
+	LLi a, b, c, gd, ans=0;
+	vector<int> v;
+
+	cin>> a >> b >> c;
+
+	gd=__gcd(a, b);
+	gd=__gcd(gd, c);
+
+	ans+=a/gd-1;
+	ans+=b/gd-1;
+	ans+=c/gd-1;
+
+	cout<< ans <<endl;
+ 
+	return 0;
+}
