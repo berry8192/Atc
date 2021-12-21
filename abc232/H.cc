2b1444e06f200e2e3230585984b13ce2ccaf0454
berry8192 <berryrt8192.gmail.com>
2021-12-21 22:31:42 +0900

    try232h

--- abc232/H.cc
+++ abc232/H.cc
@@ -1,54 +1,45 @@
 #include <bits/stdc++.h>
-
-#define rep(i, n) for (int i = 0; i < (int)(n); i++)
-#define rep3(i, n, m) for (int i = m; i < (int)(n); i++)
-#define repr(i, n) for (int i = n-1; i >= 0; i--)
-#define all(v) v.begin(), v.end()
-#define SP << " " 
-#define LLi long long int
-
 using namespace std;
 
-int imax=2147483647;
-long long int llimax=9223372036854775807;
-LLi mod=1000000007;
-//LLi mod=998244353;
+int a, b, ga, gb;
+vector< vector<int> > board;
 
-//int型vectorを出力
-template <class T> void PV(T pvv) {
-	if(!pvv.size()) return;
-	rep(i, pvv.size()-1) cout << pvv[i] SP;
-	cout<< pvv[pvv.size()-1] <<endl;
-}
+void dfs(int h, int w){
+	//移動してきた時の処理
+	//if(seen[h][w]) return;
+	board[h][w]=1;
+	cout<< h+1 << " " << w+1 <<endl;
+	if(h==ga && w==gb) return;
 
-//LLi型vectorを出力
-template <class T>void PVV(T pvv) {
-	rep(i, pvv.size()){
-		rep(j, pvv[i].size()){
-			cout << pvv[i][j] SP;
+	//移動する処理
+	vector< tuple<int, double, int, int> > dis;
+	for(int i=-1;i<=1;i++){
+		for(int j=-1;j<=1;j++){
+			if(i==0 && j==0) continue;
+			if(-1<h+i && h+i<a && -1<w+j && w+j<b){
+				if(board[h+i][w+j]) continue;
+				//int tmp=abs(h+i-ga)*(h+i-ga)*(h+i-ga)+abs(w+j-gb)*(w+j-gb)*(w+j-gb);
+				int tmp=(h+i-ga)*(h+i-ga)+(w+j-gb)*(w+j-gb);
+				double tmp2=(h+i-(a-0)/2.0)*(h+i-(a-0)/2.0)+(w+j-(b-0)/2.0)*(w+j-(b-0)/2.0);
+				//cout<< tmp2 <<endl;
+				//int tmp=abs(h+i-ga)+abs(w+j-gb);
+				dis.push_back({-tmp, -tmp2, h+i, w+j});
+			}
 		}
-		cout << endl;
 	}
+	sort(dis.begin(), dis.end());
+	dfs(get<2>(dis[0]), get<3>(dis[0]));
 }
 
 int main(){
 
-	int n, ans=0;
-	vector<int> v;
+	cin>> a >> b >> ga >> gb;
+	ga--;
+	gb--;
+	board.resize(a);
+	for(int i=0;i<a;i++) board[i].resize(b);
 
-	cin>> n;
-	v.resize(n);
+	dfs(0, 0);
 
-	rep(i, n) cin >> v[i];
-	sort(all(v));
-	PV(v);
-
-	rep(i, n) {
-		if (v[i]) ans++;
-	}
-
-	if(n==0) cout<< "Yes" << endl;
-	else cout<< "No" << endl;
- 
 	return 0;
 }
