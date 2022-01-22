d2e6c9dcb513997777655ca4744c01883c5cdc3f
berry8192 <berryrt8192.gmail.com>
2022-01-22 14:19:42 +0900

    015ac

---
+++ tenkei90/AC/015.cc
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep3(i, n, m) for (int i = m; i < (int)(n); i++)
+#define repr(i, n) for (int i = n-1; i >= 0; i--)
+#define all(v) v.begin(), v.end()
+#define SP << " " 
+#define LLi long long int
+
+using namespace std;
+
+int imax=2147483647;
+long long int llimax=9223372036854775807;
+LLi mod=1000000007;
+const int MAX = 110000;
+long long fac[MAX], finv[MAX], inv[MAX];
+
+//LLi mod=998244353;// テーブルを作る前処理
+void COMinit() {
+    fac[0] = fac[1] = 1;
+    finv[0] = finv[1] = 1;
+    inv[1] = 1;
+    for (int i = 2; i < MAX; i++){
+        fac[i] = fac[i - 1] * i % mod;
+        inv[i] = mod - inv[mod%i] * (mod / i) % mod;
+        finv[i] = finv[i - 1] * inv[i] % mod;
+    }
+}
+
+// 二項係数計算
+long long COM(int n, int k){
+    if (n < k) return 0;
+    if (n < 0 || k < 0) return 0;
+    return fac[n] * (finv[k] * finv[n - k] % mod) % mod;
+}
+
+int main(){
+    // 前処理
+    COMinit();
+
+	int n;
+	LLi ans=0;
+
+	cin>> n;
+
+	for(int k=1;k<=n;k++) {
+		ans=0;
+		for(int a=1;a<=(n+k-1)/k;a++){
+			ans+=COM(n-(k-1)*(a-1), a);
+			ans%=mod;
+		}
+		cout<< ans <<endl;
+	}
+ 
+	return 0;
+}
