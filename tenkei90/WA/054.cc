0c305a44dbabc8021339e0fd7d1e38a28993f5e9
berry8192 <berryrt8192.gmail.com>
2022-02-12 20:55:16 +0900

    053, 054wa

---
+++ tenkei90/WA/054.cc
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep3(i, n, m) for (int i = m; i < (int)(n); i++)
+#define repr(i, n) for (int i = n-1; i >= 0; i--)
+#define all(v) v.begin(), v.end()
+#define SP << " " 
+#define LLi long long int
+
+using namespace std;
+
+int imax=2147483647;
+long long int llimax=9223372036854775807;
+LLi mod=1000000007;
+//LLi mod=998244353;
+
+//int型vectorを出力
+template <class T> void PV(T pvv) {
+	rep(i, pvv.size()) cout << pvv[i] SP;
+	cout << endl;
+}
+
+//LLi型vectorを出力
+template <class T>void PVV(T pvv) {
+	rep(i, pvv.size()){
+		rep(j, pvv[i].size()){
+			cout << pvv[i][j] SP;
+		}
+		cout << endl;
+	}
+}
+
+int main(){
+
+	int n, m, tmp, r;
+	vector< vector<int> > v;
+	vector<int> ans;
+
+	cin>> n >> m;
+	v.resize(m);
+	ans.resize(n);
+
+	rep(i, m){
+		cin>> tmp;
+		rep(j, tmp){
+			cin >> r;
+			v[i].push_back(r-1);
+		}
+	}
+
+	if(n==0) cout<< "Yes" << endl;
+	else cout<< "No" << endl;
+ 
+	return 0;
+}
