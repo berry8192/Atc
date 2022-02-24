a03f84305b61124fadc04d7c86e7d6e5db8c5d35
berry8192 <berryrt8192.gmail.com>
2022-02-24 20:08:41 +0900

    進数変換のライブラリもってきた

--- lib20200513/進数変換.cc
+++ lib20200513/進数変換.cc
@@ -13,127 +13,52 @@ int imax=2147483647;
 LLi llimax=9223372036854775807;
 LLi mod=1000000007;
 
-int ntodec(const char c){
-    switch(c){
-        case '0': return 0;
-        case '1': return 1;
-        case '2': return 2;
-        case '3': return 3;
-        case '4': return 4;
-        case '5': return 5;
-        case '6': return 6;
-        case '7': return 7;
-        case '8': return 8;
-        case '9': return 9;
-        case 'A': return 10;
-        case 'B': return 11;
-        case 'C': return 12;
-        case 'D': return 13;
-        case 'E': return 14;
-        case 'F': return 15;
-        case 'G': return 16;
-        case 'H': return 17;
-        case 'I': return 18;
-        case 'J': return 19;
-        case 'K': return 20;
-        case 'L': return 21;
-        case 'M': return 22;
-        case 'N': return 23;
-        case 'O': return 24;
-        case 'P': return 25;
-        case 'Q': return 26;
-        case 'R': return 27;
-        case 'S': return 28;
-        case 'T': return 29;
-        case 'U': return 30;
-        case 'V': return 31;
-        case 'W': return 32;
-        case 'X': return 33;
-        case 'Y': return 34;
-        case 'Z': return 35;
-        default : return -1;
+class Radix {
+private:
+  const char* s;
+  int a[128];
+public:
+  Radix(const char* s = "0123456789ABCDEF") : s(s) {
+    int i;
+    for(i = 0; s[i]; ++i)
+      a[(int)s[i]] = i;
+  }
+  std::string to(long long p, int q) {
+    int i;
+    if(!p)
+      return "0";
+    char t[64] = { };
+    for(i = 62; p; --i) {
+      t[i] = s[p % q];
+      p /= q;
     }
-}
+    return std::string(t + i + 1);
+  }
+  std::string to(const std::string& t, int p, int q) {
+    return to(to(t, p), q);
+  }
+  long long to(const std::string& t, int p) {
+    int i;
+    long long sm = a[(int)t[0]];
+    for(i = 1; i < (int)t.length(); ++i)
+      sm = sm * p + a[(int)t[i]];
+    return sm;
+  }
+};
 
-char decton(const int n){
-    switch(n){
-        case 0: return '0';
-        case 1: return '1';
-        case 2: return '2';
-        case 3: return '3';
-        case 4: return '4';
-        case 5: return '5';
-        case 6: return '6';
-        case 7: return '7';
-        case 8: return '8';
-        case 9: return '9';
-        case 10: return 'A';
-        case 11: return 'B';
-        case 12: return 'C';
-        case 13: return 'D';
-        case 14: return 'E';
-        case 15: return 'F';
-        case 16: return 'G';
-        case 17: return 'H';
-        case 18: return 'I';
-        case 19: return 'J';
-        case 20: return 'K';
-        case 21: return 'L';
-        case 22: return 'M';
-        case 23: return 'N';
-        case 24: return 'O';
-        case 25: return 'P';
-        case 26: return 'Q';
-        case 27: return 'R';
-        case 28: return 'S';
-        case 29: return 'T';
-        case 30: return 'U';
-        case 31: return 'V';
-        case 32: return 'W';
-        case 33: return 'X';
-        case 34: return 'W';
-        case 35: return 'Z';
-        default : return '\0';
-    }
-}
+int main() {
+    Radix r;
+    // 10 進数を n 進文字列に
+    std::cout << r.to(255, 10) << std::endl; // => "255"
+    std::cout << r.to(255, 12) << std::endl; // => "193"
+    std::cout << r.to(255, 16) << std::endl; // => "FF"
 
-inline long long pow_ll(long long x, long long n){
-    long long ret = x;
-    if(n==0) return 1;
-    for(long long i=1; i<n; i++){
-        ret *= x;
-    }
-    return ret;
-}
+    // n 進文字列を 10 進数に
+    std::cout << r.to("255", 10) << std::endl; // => 255
+    std::cout << r.to("255", 14) << std::endl; // => 467
 
-string n_ary(string str, int n, int m){
-    unsigned long tmp = 0;
-    string ret;
-
-    for(int i=0; i<str.length(); i++){
-        tmp += (unsigned long) ntodec(str[str.length()-1-i]) * pow_ll(n, i);
-    }
-
-    if(tmp==0) return "0";
-    while(tmp!=0){
-        ret = decton(tmp%m) + ret;
-        tmp/=m;
-    }
-    return ret;
-}
-
-int main(){
-
-	int k;
-	cin>> k;
-	string a, b;
-	cin>> a >> b;
-	LLi aa, bb;
-
-	aa=stoi(n_ary(a, k, 10));
-	bb=stoi(n_ary(b, k, 10));
-
-	cout<< aa*bb << endl;
- 
-	return 0;
+    // n 進文字列を m 進文字列に
+    std::cout << r.to("255", 7, 11) << std::endl; // => "116"
+    std::cout << r.to("255", 11, 7) << std::endl; // => "611"
+    return 0;
 }
