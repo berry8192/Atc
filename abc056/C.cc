2a4136849977b7618dbd51955aed614ff15a0990
berry8192 <berryrt8192.gmail.com>
2022-03-17 19:58:58 +0900

    abc056

---
+++ abc056/C.cc
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep3(i, n, m) for (int i = m; i < (int)(n); i++)
+#define repr(i, n) for (int i = n-1; i >= 0; i--)
+#define all(v) v.begin(), v.end()
+#define SP << " " 
+#define LLi long long int
+
+using namespace std;
+
+int imax=2147483647;
+long long int llimax=9223372036854775807;
+LLi mod=1000000007;
+//LLi mod=998244353;
+
+//int型vectorを出力
+template <class T> void PV(T pvv) {
+	if(!pvv.size()) return;
+	rep(i, pvv.size()-1) cout << pvv[i] SP;
+	cout<< pvv[pvv.size()-1] <<endl;
+}
+
+//LLi型vectorを出力
+template <class T>void PVV(T pvv) {
+	rep(i, pvv.size()){
+		rep(j, pvv[i].size()){
+			cout << pvv[i][j] SP;
+		}
+		cout << endl;
+	}
+}
+
+int main(){
+
+	int x, i=1;
+
+	cin>> x;
+	
+	while(1){
+		x-=i;
+		if(x<=0){
+			cout<< i <<endl;
+			return 0;
+		}
+		i++;
+	}
+
+	return 0;
+}
