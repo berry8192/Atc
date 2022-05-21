4483166feade24133e9f677bd4d46280f7d14934
berry8192 <berryrt8192.gmail.com>
2022-05-21 23:03:03 +0900

    abc252

---
+++ abc252/D.cc
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+//#include <atcoder/all>
+
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep3(i, n, m) for (int i = m; i < (int)(n); i++)
+#define repr(i, n) for (int i = n-1; i >= 0; i--)
+#define all(v) v.begin(), v.end()
+#define SP << " " 
+#define ll long long int
+
+using namespace std;
+//using namespace atcoder;
+
+int imax=2147483647;
+ll lmax=9223372036854775807;
+//using mint = modint1000000007;
+//using mint = modint=998244353;
+
+//int型vectorを出力
+template <class T> void PV(T pvv) {
+	if(!pvv.size()) return;
+	rep(i, pvv.size()-1) cout << pvv[i] SP;
+	cout<< pvv[pvv.size()-1] <<endl;
+}
+
+//LLi型vectorを出力
+template <class T>void PVV(T pvv) {
+	rep(i, pvv.size()){
+		rep(j, pvv[i].size()){
+			cout << pvv[i][j] SP;
+		}
+		cout << endl;
+	}
+}
+
+int main(){
+
+	ll n, tmp;
+	ll ans;
+	vector<ll> du;
+
+	cin>> n;
+	ans=n*(n-1)*(n-2)/6;
+	du.resize(200010);
+
+	rep(i, n){
+		cin >> tmp;
+		du[tmp]++;
+	}
+
+	rep(i, 200010) {
+		//ll tmp2=0, tmp3=0;
+		if (du[i]>=2){
+			ans-=du[i]*(du[i]-1)/2*(n-du[i]);
+		}
+		if (du[i]>=3){
+			ans-=du[i]*(du[i]-1)*(du[i]-2)/6;
+		}
+	}
+
+	cout<< ans << endl;
+ 
+	return 0;
+}
