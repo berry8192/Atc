4182496721d81852aac5e5b72f078ef5d727b8a3
berry8192 <berryrt8192.gmail.com>
2022-06-19 22:56:30 +0900

    shell作成

---
+++ finish.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+echo "git add $1"
+git add $1
+echo "git commit -m $1"
+git commit -m $1
+echo "git push -u origin $1"
+git push -u origin $1
+echo "cd /home/berry/prj/Atc"
+cd /home/berry/prj/Atc
+
+echo "waiting PRmerge"
+read tmp
+echo "git co master"
+git co master
+echo "git pull"
+git pull
---
+++ ready.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+echo "cd /home/berry/prj/Atc"
+cd /home/berry/prj/Atc
+echo "git co -b $1"
+git co -b $1
+echo "cp -r abctmp $1"
+cp -r abctmp $1
