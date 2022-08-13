547e3ad2c8b04635ab1b254c76b58d334ff12f8a
berry8192 <berryrt8192.gmail.com>
2022-08-13 14:31:54 +0900

    unionfind実装

--- ahc013/rand.cc
+++ ahc013/rand.cc
@@ -25,6 +25,39 @@ int seed=10;
 mt19937 mt(seed);
 
 // 構造体
+struct UnionFind {
+    vector<int> par; // par[i]:iの親の番号　(例) par[3] = 2 : 3の親が2
+	vector<int> siz; // siz[i]:iの属する木に含まれる点の数
+
+    void init(int N){ //最初は全てが根であるとして初期化
+        par.resize(N);
+        for(int i = 0; i < N; i++) par[i] = i;
+		siz.resize(par.size(), 1); //最初は全てサイズ1
+    }
+
+    int root(int x) { // データxが属する木の根を再帰で得る：root(x) = {xの木の根}
+        if (par[x] == x) return x;
+        return par[x] = root(par[x]);
+    }
+
+	int tsize(int x){ // siz[x]の木の根を見に行き、サイズを返す
+		return siz[root(x)];
+	}
+
+    void unite(int x, int y) { // xとyの木を併合
+        int rx = root(x); //xの根をrx
+        int ry = root(y); //yの根をry
+        if (rx == ry) return; //xとyの根が同じ(=同じ木にある)時はそのまま
+        if(siz[rx]<siz[ry]) swap(rx,ry);
+        par[ry] = rx; //xとyの根が同じでない(=同じ木にない)時：xの根rxをyの根ryにつける
+		siz[rx]+=siz[ry]; //根で管理
+    }
+
+    bool same(int x, int y){
+        return root(x)==root(y);
+    }
+};
+
 struct Pos{
     int h;
     int w;
@@ -39,9 +72,24 @@ struct Pos{
     }
 };
 
+struct Num{
+    int type;
+    int idx;
+
+    Num(){};
+    Num(int x, int y){
+        type=x;
+        idx=y;
+    }
+    void print(){
+        if(type>=0) printf("%02d", type);
+        else cout<< type SP;
+    }
+};
+
 //入力
 int n, k;
-vector<vector<int>> c;
+vector<vector<Num>> c;
 vector<vector<Pos>> cpu;
 
 struct Move{
@@ -77,10 +125,11 @@ struct Cone{
 };
 
 struct Room{
-    vector<vector<int>> board;
+    vector<vector<Num>> board;
     vector<vector<Pos>> comp;
     vector<Move> mv;
     vector<Cone> co;
+    UnionFind uf;
     int score=0;
 
     Room(){
@@ -92,6 +141,12 @@ struct Room{
     void init(){
         board=c;
         comp=cpu;
+        uf.init(k*100);
+        rep(i, n){
+            rep(j, n){
+                if(board[i][j].type>0) cout<< board[i][j]
+            }
+        }
     }
     int hand(){
         return mv.size()+co.size();
@@ -102,8 +157,8 @@ struct Room{
     }
     void add_co(int x1, int y1, int x2, int y2){
         // cout<< x1 SP << y1 SP << x2 SP << y2 <<endl;
-        int from=board[x1][y1];
-        int to=board[x2][y2];
+        int from=board[x1][y1].type;
+        int to=board[x2][y2].type;
         // for文のために右か下方向に伸ばすようにする
         if(x1>x2) swap(x1, x2);
         if(y1>y2) swap(y1, y2);
@@ -117,30 +172,73 @@ struct Room{
         if(x1!=x2){
             // 間をケーブルが通っていないか確認
             rep3(i, x2, x1+1){
-                // assert(board[i][y1]==0);
-                if(board[i][y1]!=0) return;
+                assert(board[i][y1].type==0);
             }
             rep3(i, x2, x1+1){
-                board[i][y1]=-from;
+                board[i][y1].type=-from;
             }
         }else{
             // 間をケーブルが通っていないか確認
             rep3(i, y2, y1+1){
-                // assert(board[x1][i]==0);
-                if(board[x1][i]!=0) return;
+                assert(board[x1][i].type==0);
             }
             rep3(i, y2, y1+1){
-                board[x1][i]=-from;
+                board[x1][i].type=-from;
             }
         }
         // coに追加
         co.push_back({{x1, y1}, {x2, y2}});
+        uf.unite(board[x1][y1].idx, board[x2][y2].idx);
     }
+    bool between_zero(int x1, int y1, int x2, int y2){
+        if(x1!=x2){
+            rep3(i, x2, x1+1){
+                if(board[i][y1].type!=0) return false;
+            }
+        }else{
+            rep3(i, y2, y1+1){
+                if(board[x1][i].type!=0) return false;
+            }
+        }
+        return true;
+    }
+    void nomove_connect(int num, int length){
+        rep(i, n){
+            rep(j, n){
+                int tmp=board[i][j].type;
+                if(tmp!=num) continue;
+                // if(tmp>1) continue;
+                rep3(l, min(i+1+length, n), i+1){
+                    if(board[l][j].type==0){
+                        continue;
+                    }else if(board[l][j].type==tmp){
+                        if(!uf.same(board[i][j].idx, board[l][j].idx) && between_zero(i, j, l, j)) add_co(i, j, l, j);
+                        break;
+                    }else{
+                        break;
+                    }
+                }
+                rep3(l, min(j+1+length, n), j+1){
+                    if(board[i][l].type<=0){
+                        continue;
+                    }else if(board[i][l].type==tmp){
+                        if(!uf.same(board[i][j].idx, board[i][l].idx) && between_zero(i, j, i, l)) add_co(i, j, i, l);
+                        break;
+                    }else{
+                        break;
+                    }
+                }
+            }
+        }
+    }
+    // bool board_connect(int x1, int y1, int x2, int y2){
+        
+    // }
     void print_board(){
         rep(i, n){
             rep(j, n){
-                if(board[i][j]>0) printf("%02d ", board[i][j]);
-                else cout<< board[i][j] SP;
+                board[i][j].print();
+                cout SP;
             }
             cout<< endl;
         }
@@ -164,25 +262,36 @@ void inpt(){
         cin>> tmp;
         rep(j, n){
             int fig=int(tmp[j]-'0');
-            c[i][j]=fig;
+            c[i][j]=Num(fig, i*n+j);
             if(fig){
                 cpu[fig-1].push_back({i, j});
             }
         }
     }
+    // rep(i, k){
+    //     rep(j, 100){
+    //         cpu[i][j].print();
+    //         cout SP;
+    //     }
+    //     cout<< endl;
+    // }
 }
 
-int score(Room room){
-    vector<vector<int>> cc=c;
-    rep(i, room.mv.size()){
-        assert(room.board[room.mv[i].to.h][room.mv[i].to.w]==0);
-        assert(room.board[room.mv[i].from.h][room.mv[i].from.w]>0);
-        swap(room.board[room.mv[i].to.h][room.mv[i].to.w], room.board[room.mv[i].from.h][room.mv[i].from.w]);
-    }
-    rep(i, room.co.size()){
-        room.add_co(room.co[i].from.h, room.co[i].from.w, room.co[i].to.h, room.co[i].to.w);
-    }
-}
+// int score(Room room){
+//     vector<vector<Num>> cc=c;
+//     rep(i, room.mv.size()){
+//         int th=room.mv[i].to.h;
+//         int tw=room.mv[i].to.w;
+//         int fh=room.mv[i].from.h;
+//         int fw=room.mv[i].from.w;
+//         assert(room.board[th][tw].type==0);
+//         assert(room.board[fh][fw].type>0);
+//         swap(room.board[th][tw], room.board[fh][fw]);
+//     }
+//     rep(i, room.co.size()){
+//         room.add_co(room.co[i].from.h, room.co[i].from.w, room.co[i].to.h, room.co[i].to.w);
+//     }
+// }
 
 int main(){
     //開始時間の計測
@@ -194,69 +303,20 @@ int main(){
     Room cur, best;
     cur.init();
 
-    rep(i, n){
-        rep(j, n){
-            if(cur.board[i][j]<=0) continue;
-            int tmp=cur.board[i][j];
-            rep3(l, n, i+1){
-                if(cur.board[l][j]==0){
-                    continue;
-                }else if(cur.board[l][j]==tmp){
-                    cur.add_co(i, j, l, j);
-                    break;
-                }else{
-                    break;
-                }
-            }
-            rep3(l, n, j+1){
-                if(cur.board[i][l]<=0){
-                    continue;
-                }else if(cur.board[i][l]==tmp){
-                    cur.add_co(i, j, i, l);
-                    break;
-                }else{
-                    break;
-                }
-            }
-        }
-    }
+    // cur.nomove_connect(1);
+    // cur.nomove_connect(2);
+    // cur.nomove_connect(3);
+    // cur.nomove_connect(4);
+    // cur.nomove_connect(5);
+    rep(i, k) cur.nomove_connect(i+1, 100);
 
-    // // 焼きなまし
-    // int lp=0;
-    // while (true){
-    //     lp++;
-    //     //cout<< lp <<endl;
-    //     current = chrono::system_clock::now();
-    //     if (chrono::duration_cast<chrono::milliseconds>(current - start).count() > TIME_LIMIT) break;
-
-    //     int type=mt()%100;
-    //     if(type<90){
-    //         // コンピュータを配置しなおす
-
-    //     }else if(type<100){
-    //         // 単純に線を削除
-    //     }else{
-    //         // 
-    //     }
-
-    //     // 温度関数
-    //     double temp = start_temp + (end_temp - start_temp) * chrono::duration_cast<chrono::milliseconds>(current - start).count() / TIME_LIMIT;
-    //     // 遷移確率関数(最大化の場合)
-    //     double prob = exp((cur.score-best.score)/temp);
-
-    //     if (prob > (mt()%imax)/(double)imax) { // 確率probで遷移する
-    //         best=cur;
-    //     }else{
-    //         if(type==1){
-                
-    //         }else{
-                
-    //         }
-    //     }
-    // }
-
-    // cur.print_board();
+    cur.print_board();
     cur.print_out();
 
 	return 0;
 }
+
+// uf木セット
+// Room.score関数作成
+// 通り道の邪魔な別種CPUをどかす処理
+// 近くでたむろしている同種CPUの接続
