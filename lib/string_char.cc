23a0db0b471afbef84043b7bb025d48a8619a802
berry8192 <berryrt8192.gmail.com>
2022-10-06 20:46:05 +0900

    rleのバグ修正

--- lib/string_char.cc
+++ lib/string_char.cc
@@ -6,7 +6,7 @@ using namespace std;
 void rle(string s, vector<pair<char, int>> &vec)
 {
   int cnt = 1;
-  for(int i = 0; i < (int)s.size(); i++){
+  for(int i = 1; i < (int)s.size(); i++){
     if(s[i] != s[i-1]){
       vec.push_back({s[i-1], cnt});
       cnt = 0;
