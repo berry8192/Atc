efe444f4ddd617aa515bc633b18a174eebfb4da8
berry8192 <berryrt8192@gmail.com>
2022-12-10 23:10:59 +0900

    シェルのやつを変更

--- ready.sh
+++ ready.sh
@@ -1,12 +1,3 @@
 #!/bin/sh
 
-echo "cd /home/berry/prj/Atc"
-cd /home/berry/prj/Atc
-sleep 1
-echo "git co -b $1"
-git co -b $1
-echo "cp -r abctmp $1"
-cp -r abctmp $1
-sleep 1
-echo "cd $1"
-cd $1
+echo "git co -b $1" && echo "cp -r abctmp $1" && cp -r abctmp $1 && echo "cd $1"
