0e3e16271cb67a2f50c1fb44fce73e800fa87694
berry8192 <berryrt8192@gmail.com>
2023-01-30 23:00:21 +0900

    頂点数調査

--- ahc017/input_agg/input_agg.sh
+++ ahc017/input_agg/input_agg.sh
@@ -1,5 +1,6 @@
 echo "N, M, M/N, D, K, M/D, dist_sum" > input_agg_log.csv
 sleep 1
-for i in {0000..0099}; do
-    ./a.out < ../testcases/${i}.txt >> input_agg_log.csv
+for i in {0000..1999}; do
+    # ./a.out < ../testcases/${i}.txt >> input_agg_log.csv
+    ./a.out < ../testcases/${i}.txt >> log.txt
 done
