a65b446764e22b29db3649234289226b7ab78157
berry8192 <berryrt8192@gmail.com>
2023-02-20 03:05:27 +0900

    NWKCも集計

---
+++ ahc018/NWKC.cc
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define SP << ", " 
+
+int N, W, K, C;
+
+int main(){
+    cin>> N >> W >> K >> C;
+    cout<< N SP << W SP << K SP << C <<endl;
+
+    return 0;
+}
