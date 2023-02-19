413d630360500629c57ee736990d83278b97bfe9
berry8192 <berryrt8192@gmail.com>
2023-02-19 20:44:01 +0900

    Pos構造体を作成、謎初期解を削除

--- ahc018/A.cc
+++ ahc018/A.cc
@@ -18,7 +18,6 @@ using namespace std;
 int INF=1000000000;
 int imax=2147483647;
 ll lmax=9223372036854775807;
-int dr[4]={0, -1, 0, 1}, dc[4]={-1, 0, 1, 0};
 
 //焼きなましの定数
 double TIME_LIMIT=4900.0;
@@ -34,6 +33,7 @@ int lp=0;
 // int excavation_count=0; //testtest
 // int score=0; //testtest
 
+// 構造体
 struct UnionFind {
     vector<int> par; // par[i]:iの親の番号　(例) par[3] = 2 : 3の親が2
 
@@ -59,6 +59,69 @@ struct UnionFind {
         return rx == ry;
     }
 };
+
+struct Pos{
+    int x;
+    int y;
+
+    Pos(){};
+    Pos(int xx, int yy){
+        x=xx;
+        y=yy;
+    }
+    // int weight(){
+    //     //cout<< "weight" <<endl;
+    //     return (x-n/2)*(x-n/2)+(y-n/2)*(y-n/2)+1;
+    // }
+    bool is_out_of_bounce(){
+        //cout<< "out_of_bounce" <<endl;
+        return (x<0 || 199<x || y<0 || 199<y);
+    }
+    int manhattan(Pos a){
+        //cout<< "manhattan" <<endl;
+        if(a.is_out_of_bounce()){
+            a.print();
+            assert(!a.is_out_of_bounce());
+        }
+        if(is_out_of_bounce()){
+            print();
+            assert(!is_out_of_bounce());
+        }
+        return (abs(a.x-x)+abs(a.y-y));
+    }
+
+    void print(){
+        cout<< "(" << x << ", " << y << ")";
+    }
+    friend ostream &operator<<(ostream &os, const Pos &d) {
+        os << d.y << " " << d.x;
+        return os;
+    }
+
+    Pos operator+(const Pos pos){
+        Pos rtn;
+        rtn.x=x+pos.x;
+        rtn.y=y+pos.y;
+        return rtn;
+    }
+    Pos operator-(const Pos pos){
+        Pos rtn;
+        rtn.x=x-pos.x;
+        rtn.y=y-pos.y;
+        return rtn;
+    }
+    void operator+=(const Pos pos){
+        x+=pos.x;
+        y+=pos.y;
+    }
+    bool operator<(const Pos &in) const{
+		return x!=in.x ? x<in.x : y<in.y;
+	};
+    bool operator!=(const Pos &in) const{
+		return x!=in.x && y!=in.y;
+	};
+};
+Pos d4[]={{0, 1}, {-1, 0}, {0, -1}, {1, 0}};
 
 //グローバル
 int N, W, K, C;
@@ -98,10 +161,10 @@ void merge_uf(int y, int x){
     // 繋がっていればつなげる
     int id=y*N+x;
     rep(i, 4){
-        if(y+dr[i]<0 || N<=y+dr[i] || x+dc[i]<0 || N<=x+dc[i]){
+        if(y+d4[i].y<0 || N<=y+d4[i].y || x+d4[i].x<0 || N<=x+d4[i].x){
             continue;
         }
-        int nid=(y+dr[i])*N+x+dc[i];
+        int nid=(y+d4[i].y)*N+x+d4[i].x;
         uf.unite(id, nid);
     }
 }
@@ -170,23 +233,7 @@ int main(){
 
     init();
 
-    rep(i, 200){
-        rep(j, 200){
-            if(i==c[0] && j==d[0]) continue;
-            while(1){
-                cout<< i SP << j SP << 100 <<endl;
-                int tmp;
-                cin>> tmp;
-                if(tmp==1) break;
-            }
-        }
-    }
-    while(1){
-        cout<< c[0] SP << d[0] SP << 100 <<endl;
-        int tmp;
-        cin>> tmp;
-        if(tmp==2) break;
-    }
+
 
     return 0;
 }
