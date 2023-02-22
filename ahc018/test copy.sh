fbe6b0d654a615fe6375f4cfa71cf7982d9aab66
berry8192 <berryrt8192@gmail.com>
2023-02-23 03:01:47 +0900

    20並列で回して答えを1ファイルに書き出すようにする

--- ahc018/test copy.sh
+++ ahc018/test copy.sh
@@ -1,6 +1,21 @@
 DATE=`date '+%Y%m%d%H%M%S'`
 mkdir ../test_results/${DATE}
 sleep 1
-for i in {0000..2999}; do
-    cargo run --release --bin tester ../a.out < ../testcases/${i}.txt 1> ../test_results/${DATE}/${i}.txt 2>> ../test_results/${DATE}/${DATE}.txt
+CORES=20
+START=0
+END=2999
+for ((i=START; i<=END; i++)); do
+    i4=$(printf "%04d" $i)
+    cargo run --release --bin tester ../a.out < ../testcases/${i4}.txt 1> ../test_results/${DATE}/${i4}.txt 2>> ../test_results/${DATE}/${i4}_score.txt &
+    echo ${i4}" done"
+
+    if [[ $(jobs -r -p | wc -l) -ge ${CORES} ]]; then
+        wait -n # 最も古いプロセスが終了するのを待機する
+    fi
+done
+sleep 1
+
+for ((i=START; i<=END; i++)); do
+    i4=$(printf "%04d" $i)
+    sed -n '$p' ../test_results/${DATE}/${i4}_score.txt | sed -e 's/.*= //; s/ //g' >> ../test_results/${DATE}/score_sum.txt
 done
