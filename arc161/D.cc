794bb84b146af0289c8a6a7440f613cd1d06e24f
berry8192 <berryrt8192@gmail.com>
2023-05-28 23:01:04 +0900

    arc161

---
+++ arc161/D.cc
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+//#include <atcoder/all>
+
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep3(i, n, m) for (int i = m; i < (int)(n); i++)
+#define repr(i, n) for (int i = n-1; i >= 0; i--)
+#define all(v) v.begin(), v.end()
+#define SP << " " 
+#define ll long long
+
+using namespace std;
+//using namespace atcoder;
+
+int imax=2147483647;
+ll lmax=9223372036854775807;
+//using mint = modint1000000007;
+//using mint = modint=998244353;
+
+//int型vectorを出力
+template <class T> void PV(T pvv) {
+	if(!pvv.size()) return;
+	rep(i, pvv.size()-1) cout << pvv[i] SP;
+	cout<< pvv[pvv.size()-1] <<endl;
+}
+
+//LLi型vectorを出力
+template <class T>void PVV(T pvv) {
+	rep(i, pvv.size()){
+		rep(j, pvv[i].size()){
+			cout << pvv[i][j] SP;
+		}
+		cout << endl;
+	}
+}
+
+int main(){
+
+	ll n, d;
+	cin>> n >> d;
+
+	if(n*(n-1)/2<n*d){
+		cout<< "No" <<endl;
+		return 0;
+	}
+
+	cout<< "Yes" <<endl;
+	int cnt=0;
+	rep3(i, n, 1){
+		rep(j, n){
+			if(cnt>=n*d) break;
+			cout<< j+1 SP << (j+1-1+i)%n+1 <<endl;
+			cnt++;
+		}
+	}
+ 
+	return 0;
+}
