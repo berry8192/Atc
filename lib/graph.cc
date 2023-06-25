e84422346af129d25987a63de8a6f239e32bc5e7
berry8192 <berryrt8192@gmail.com>
2023-06-25 14:32:45 +0900

    graph.ccに切り出し

--- lib/graph.cc
+++ lib/graph.cc
@@ -13,7 +13,6 @@ int imax=2147483647;
 ll lmax=9223372036854775807;
 ll mod=998244353;
 // ll mod=1000000007;
-
 struct edge{
 	int to;
 	int co;
@@ -22,7 +21,6 @@ struct edge{
 		return co!=in.co ? co<in.co : to<in.to;
 	};
 };
-
 // クラスカル法とかやりたいときに使う
 struct s_edge{
 	int to;
@@ -33,93 +31,35 @@ struct s_edge{
 		return co!=in.co ? co<in.co : to<in.to ? to<in.to : from<in.from;
 	};
 };
+struct Graph{
+	vector<vector<edge>> g;
+	vector<s_edge> edges;
 
-// 1-indexにします
-vector<vector<edge>> g;
-vector<ll> dis;
-vector<int> seen;
-// vector<s_edge> g;
-// map<int, vector<edge>> g; //配列だとMLEする場合はこっち
-queue<int> que;
-ll ans=0;
+	void init(int nn, int mm, bool weight=false, bool directed=false){
+		// n頂点、m辺、重み有無、有向無向
+		int uu, vv, ww;
+		g.resize(nn);
 
-ll dfs(int cur){
-	if(seen[cur]){
-		return 0;
-	}
-	seen[cur]=1;
-	ll tmp=0;
-	rep(i, g[cur].size()){
-		tmp+=dfs(g[cur][i]);
-	}
-	return tmp;
-}
+		for(int i=0;i<mm;i++){
+			cin>> uu >> vv;
+			uu--;
+			vv--;
+			if(weight) cin>> ww;
+			else ww=1;
 
-void bfs(int cur){
-	if(seen[cur]){
-		return 0;
-	}
-	seen[cur]=1;
-	ans+=cur;
-	rep(i, g[cur].size()){
-		que.push(g[cur][i]);
-	}
-}
-
-//int型vectorを出力
-template <class T> void PV(T pvv) {
-	if(!pvv.size()) return;
-	rep(i, pvv.size()-1) cout << pvv[i] SP;
-	cout<< pvv[pvv.size()-1] <<endl;
-}
-
-//ll型vectorを出力
-template <class T>void PVV(T pvv) {
-	rep(i, pvv.size()){
-		rep(j, pvv[i].size()){
-			cout << pvv[i][j] SP;
+			g[uu].push_back({vv, ww});
+			edges.push_back({uu, vv, ww});
+			if(!directed){
+				g[vv].push_back({uu, ww});
+				edges.push_back({vv, uu, ww});
+			}
 		}
-		cout << endl;
 	}
-}
+};
 
 int main(){
-
-	int n, m, a, b, c;
-
+	int n, m;
 	cin>> n >> m;
-    g.resize(n+1);
-    dis.resize(n+1, lmax);
-    seen.resize(n+1);
-
-	rep(i, m){
-        cin >> a >> b;
-	    // cin >> a >> b >> c;
-
-        edge tmp;
-        tmp.to=b;
-        tmp.co=1;
-        // tmp.co=c;
-        gra[a].push_back(tmp);
-
-        edge tmpr;
-        tmpr.to=a;
-        tmpr.co=1;
-        // tmp.co=c;
-        gra[b].push_back(tmpr); // be disable directed graph
-
-        // s_edge tmps;
-        // tmps.from=a;
-        // tmps.to=b;
-        // tmps.co=c;
-        // eg.push_back(tmps);
-    }
-
-    // sort(all(eg));
-    // rep(i, n) sort(all(gra[i+1]));
-
-	if(n==0) cout<< "Yes" << endl;
-	else cout<< "No" << endl;
- 
-	return 0;
+	Graph graph;
+	graph.init(n, m, false, true);
 }
