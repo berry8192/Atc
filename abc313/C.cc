a618aaf2167cbd31a485c57c5db7045fd85d5665
berry8192 <berryrt8192@gmail.com>
2023-08-05 22:44:27 +0900

    abc313

---
+++ abc313/C.cc
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+//#include <atcoder/all>
+
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep3(i, n, m) for (int i = m; i < (int)(n); i++)
+#define repr(i, n) for (int i = n-1; i >= 0; i--)
+#define all(v) v.begin(), v.end()
+#define SP << " " 
+#define ll long long
+
+using namespace std;
+//using namespace atcoder;
+
+int imax=2147483647;
+ll lmax=9223372036854775807;
+//using mint = modint1000000007;
+//using mint = modint=998244353;
+
+//int型vectorを出力
+template <class T> void PV(T pvv) {
+	if(!pvv.size()) return;
+	rep(i, pvv.size()-1) cout << pvv[i] SP;
+	cout<< pvv[pvv.size()-1] <<endl;
+}
+
+//LLi型vectorを出力
+template <class T>void PVV(T pvv) {
+	rep(i, pvv.size()){
+		rep(j, pvv[i].size()){
+			cout << pvv[i][j] SP;
+		}
+		cout << endl;
+	}
+}
+
+int main(){
+
+	int n;
+	ll su=0, ave, u=0, d=0;
+	vector<int> v;
+
+	cin>> n;
+	v.resize(n);
+
+	rep(i, n){
+		cin >> v[i];
+		su+=v[i];
+	}
+	ave=su/n;
+
+	rep(i, n){
+		if(v[i]<ave) u+=ave-v[i];
+		if(ave+1<v[i]) d+=v[i]-(ave+1);
+	}
+
+	cout<< max(u, d) << endl;
+ 
+	return 0;
+}
