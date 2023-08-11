f0ec6aad87a95b30c99ef241785eef9a57a77388
berry8192 <berryrt8192@gmail.com>
2023-08-11 15:11:56 +0900

    型だけ作る

---
+++ ahc022/burn.cc
@@ -0,0 +1,94 @@
+#include <bits/stdc++.h>
+// #include <atcoder/all>
+
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep3(i, n, m) for (int i = m; i < (int)(n); i++)
+#define repr(i, n) for (int i = n-1; i >= 0; i--)
+#define all(v) v.begin(), v.end()
+#define SP << " " 
+#define ll long long
+
+using namespace std;
+// using namespace atcoder;
+
+int imax=2147483647;
+long long llimax=9223372036854775807;
+
+//焼きなましの定数
+double TIME_LIMIT=3900.0;
+// double TIME_LIMIT=190.0;
+double start_temp=10000000.0;
+double end_temp=10000.0;
+
+// 乱数の準備
+// auto seed=(unsigned)time(NULL);
+int seed=1;
+mt19937 mt(seed);
+
+struct Pos;
+
+int l, n, s;
+Pos yx[110];
+
+// 構造体
+struct Pos{
+    int y;
+    int x;
+
+    Pos(){};
+    Pos(int yy, int xx){
+        y=yy;
+        x=xx;
+    }
+
+    void print(){
+        cout<< "(" << y << ", " << x << ")" <<endl;
+    }
+    Pos operator+(const Pos pos){
+        Pos rtn;
+        rtn.y=y+pos.y;
+        rtn.x=x+pos.x;
+        return rtn;
+    }
+    Pos operator-(const Pos pos){
+        Pos rtn;
+        rtn.y=y-pos.y;
+        rtn.x=x-pos.x;
+        return rtn;
+    }
+};
+
+struct Space{
+
+    void init(){
+    }
+
+    ll calc_score(){
+    }
+    void print_ans(){
+    }
+};
+
+void inpt(){
+    cin>> l >> n >> s;
+
+    rep(i, n){
+        cin>> yx[i].y >> yx[i].x;
+    }
+}
+
+int main(){
+    //開始時間の計測
+    std::chrono::system_clock::time_point start, current;
+    double delta;
+    start = chrono::system_clock::now();
+
+    inpt();
+    Space space;
+    space.init();
+
+
+    space.print_ans();
+
+    return 0;
+}
