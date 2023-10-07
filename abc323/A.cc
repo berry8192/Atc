6921966b6060c8eb7161e5b086b7be6c45056065
berry8192 <berryrt8192@gmail.com>
2023-10-08 02:12:53 +0900

    abc323

---
+++ abc323/A.cc
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep3(i, n, m) for (int i = m; i < (int)(n); i++)
+#define repr(i, n) for (int i = n-1; i >= 0; i--)
+#define all(v) v.begin(), v.end()
+#define SP << " " 
+#define ll long long
+
+using namespace std;
+
+int imax=2147483647;
+ll llimax=9223372036854775807;
+ll mod=1000000007;
+
+int main(){
+
+	string s;
+	cin>> s;
+bitset<16> bs(s), b("0101010101010101");
+
+if((bs&b).any()) cout<< "No" <<endl;
+	else cout<< "Yes" << endl;
+ 
+	return 0;
+}
