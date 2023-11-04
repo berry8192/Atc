c8b5d99e08c9b6fb8b4465450ffa5e6a66b7da8a
berry8192 <berryrt8192@gmail.com>
2023-11-04 23:12:15 +0900

    abc327

---
+++ abc327/A.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int N;
+    string S;
+    cin >> N >> S;
+
+    for (int i = 0; i < N - 1; i++) {
+        if (S[i] == 'a' && S[i + 1] == 'b' || S[i] == 'b' && S[i + 1] == 'a') {
+            cout << "Yes" << endl;
+            return 0;
+        }
+    }
+
+    cout << "No" << endl;
+    return 0;
+}
