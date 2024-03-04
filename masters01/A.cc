4a48a04cdd0101dff7d496466a6c0deef3076e9e
berry8192 <berryrt8192@gmail.com>
2024-03-05 03:59:25 +0900

    複数手先を見たい

--- masters01/A.cc
+++ masters01/A.cc
@@ -88,7 +88,7 @@ template <typename T> T rand(T a, T b) {
 }
 
 // 入力
-int t, n;
+int t, n, look;
 vector<string> ve, ho;
 vector<vector<int>> a;
 
@@ -195,15 +195,67 @@ struct Turn {
     void print() { cout << s SP << d SP << e << endl; }
 };
 
+struct Neighbor {
+    Pos prev_pos;
+    int dist;
+
+    Neighbor(){};
+};
+vector<map<Pos, Neighbor>> NN;
+
+void calc_all_NN(int depth) {
+    NN.resize(n * n);
+    rep(i, n) {
+        rep(j, n) {
+            int c_index = i * n + j;
+            Pos cpos = {i, j};
+            NN[c_index][cpos].prev_pos = {-1, -1};
+            NN[c_index][cpos].dist = 0;
+            queue<Pos> qp;
+            queue<int> qd;
+            qp.push(cpos);
+            qd.push(0);
+            vector<int> seen(n * n);
+
+            while (!qd.empty()) {
+                Pos pos = qp.front();
+                int dep = qd.front();
+                qp.pop();
+                qd.pop();
+
+                seen[pos.index()] = 1;
+                vector<Pos> poses = pos.around_pos();
+                rep(k, poses.size()) {
+                    if (seen[poses[k].index()])
+                        continue;
+                    seen[poses[k].index()] = 1;
+                    NN[c_index][poses[k]].prev_pos = pos;
+                    NN[c_index][poses[k]].dist = dep + 1;
+                    if (dep + 1 >= depth)
+                        continue;
+                    qp.push(poses[k]);
+                    qd.push(dep + 1);
+                }
+            }
+
+            // for (auto it : NN[c_index]) {
+            //     Pos pos = it.first;
+            //     Neighbor neighbor = it.second;
+            //     pos.print();
+            //     neighbor.prev_pos.print();
+            //     cout << neighbor.dist << endl;
+            // }
+            // exit(0);
+        }
+    }
+}
+
 struct Grid {
     int pi, pj, qi, qj;
     Pos taka, aoki;
     vector<vector<int>> board;
-    vector<vector<int>> taka_visit, aoki_visit;
+    // vector<vector<int>> taka_visit, aoki_visit;
     vector<Turn> ans;
-
-    vector<vector<vector<int>>> NN_dir;
-    vector<vector<vector<Pos>>> NN;
 
     void init() {
         // cout << "init" << endl;
@@ -214,40 +266,8 @@ struct Grid {
         taka = {pi, pj};
         aoki = {qi, qj};
         board = a;
-        taka_visit = vector<vector<int>>(n, vector<int>(n));
-        aoki_visit = vector<vector<int>>(n, vector<int>(n));
-        NN_dir.resize(n, vector<vector<int>>(n, vector<int>()));
-        NN.resize(n, vector<vector<Pos>>(n, vector<Pos>()));
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                // d4[0]について、隣接していればNNに追加
-                if (j < n - 1 && ve[i][j] == '0')
-                    NN_dir[i][j].push_back(0);
-                // d4[2]について、隣接していればNNに追加
-                if (0 < j && ve[i][j - 1] == '0')
-                    NN_dir[i][j].push_back(2);
-                // d4[1]について、隣接していればNNに追加
-                if (0 < i && ho[i - 1][j] == '0')
-                    NN_dir[i][j].push_back(1);
-                // d4[3]について、隣接していればNNに追加
-                if (i < n - 1 && ho[i][j] == '0')
-                    NN_dir[i][j].push_back(3);
-
-                // d4[0]について、隣接していればNNに追加
-                if (j < n - 1 && ve[i][j] == '0')
-                    NN[i][j].push_back(Pos(i, j) + d4[0]);
-                // d4[2]について、隣接していればNNに追加
-                if (0 < j && ve[i][j - 1] == '0')
-                    NN[i][j].push_back(Pos(i, j) + d4[2]);
-                // d4[1]について、隣接していればNNに追加
-                if (0 < i && ho[i - 1][j] == '0')
-                    NN[i][j].push_back(Pos(i, j) + d4[1]);
-                // d4[3]について、隣接していればNNに追加
-                if (i < n - 1 && ho[i][j] == '0')
-                    NN[i][j].push_back(Pos(i, j) + d4[3]);
-            }
-        }
+        // taka_visit = vector<vector<int>>(n, vector<int>(n));
+        // aoki_visit = vector<vector<int>>(n, vector<int>(n));
         // cout << "init end" << endl;
     }
 
@@ -258,11 +278,14 @@ struct Grid {
         qj = rand(0, n - 1);
         taka = Pos(pi, pj);
         aoki = Pos(qi, qj);
+        // taka.print();
+        // aoki.print();
     }
 
     long long getDij(Pos p) {
         ll sum = 0;
-        for (auto nxt : NN[p.h][p.w]) {
+        vector<Pos> around_p = p.around_pos();
+        for (auto nxt : around_p) {
             ll diff = board[nxt.h][nxt.w] - board[p.h][p.w];
             sum += diff * diff;
         }
@@ -279,26 +302,27 @@ struct Grid {
         return sum / 2;
     }
 
-    long long exchange(Pos p1, Pos p2) {
+    long long exchange(Pos p1, Pos p2, bool execute = false) {
         ll now = getDij(p1) + getDij(p2);
         // p1とp2を交換してみる
         swap(board[p1.h][p1.w], board[p2.h][p2.w]);
         ll change = getDij(p1) + getDij(p2);
-        // cout << now SP << change << endl;
+        // if (execute)
+        // cout << change SP << now << endl;
 
         // 悪化していたらもとに戻す
-        if (now <= change) {
+        if (!execute) {
             swap(board[p1.h][p1.w], board[p2.h][p2.w]);
         }
         // どれだけ改善したか
-        return now - change;
+        return change - now;
     }
 
     void random_walk() {
         // cout << "random_walk" << endl;
         rep(i, 4 * n * n) {
-            taka_visit[taka.h][taka.w] += 3;
-            aoki_visit[aoki.h][aoki.w] += 3;
+            // taka_visit[taka.h][taka.w] += 3;
+            // aoki_visit[aoki.h][aoki.w] += 3;
             bool kaizen = (0 < exchange(taka, aoki));
             int s;
             if (kaizen)
@@ -324,7 +348,8 @@ struct Grid {
 
         auto dfs = [&](auto dfs, Pos p) -> void {
             seen[p.h * n + p.w] = 1;
-            for (auto nxt : NN[p.h][p.w]) {
+            vector<Pos> around_p = p.around_pos();
+            for (auto nxt : around_p) {
                 if (seen[nxt.h * n + nxt.w])
                     continue;
                 dfs_route.push_back(nxt);
@@ -339,8 +364,8 @@ struct Grid {
 
         // cout << "random_walk" << endl;
         rep(i, 4 * n * n) {
-            taka_visit[taka.h][taka.w] += 3;
-            aoki_visit[aoki.h][aoki.w] += 3;
+            // taka_visit[taka.h][taka.w] += 3;
+            // aoki_visit[aoki.h][aoki.w] += 3;
             // outputFile << "i, taka, aoki: " << i SP << taka.h SP << taka.w SP
             //            << aoki.h SP << aoki.w << endl;
             bool kaizen = (0 < exchange(taka, aoki));
@@ -375,13 +400,113 @@ struct Grid {
         }
     }
 
+    void NN_walk() {
+        int turn = 0;
+        while (turn < 4 * n * n) {
+            // cout << "turn_start" << endl;
+            // cout << "from" << endl;
+            // taka.print();
+            // aoki.print();
+            map<Pos, Neighbor> t_nei = NN[taka.index()];
+            map<Pos, Neighbor> a_nei = NN[aoki.index()];
+            int best_eval = -llimax;
+            Pos best_taka, best_aoki;
+            int need_turn, best_need_turn;
+            ll tmp;
+            for (auto tk : t_nei) {
+                for (auto ao : a_nei) {
+                    need_turn = max(tk.second.dist, ao.second.dist);
+                    if (turn + need_turn + 1 > 4 * n * n)
+                        continue;
+                    // cout << tk.first.h SP << tk.first.w << ", " << ao.first.h
+                    // SP
+                    //      << ao.first.w << endl;
+                    tmp = exchange(tk.first, ao.first) / (1 + need_turn);
+                    // cout << tmp << endl;
+                    // cout << tk.second.dist << ", " << ao.second.dist << endl;
+                    // cout << need_turn << endl;
+                    if (best_eval < tmp) {
+                        best_eval = tmp;
+                        best_taka = tk.first;
+                        best_aoki = ao.first;
+                        best_need_turn = need_turn;
+                    }
+                }
+            }
+
+            // cout << "kaizen, tuen: " << best_eval SP << best_need_turn <<
+            // endl; cout << "goto" << endl; best_taka.print();
+            // best_aoki.print();
+            // exit(0);
+
+            turn += best_need_turn;
+            vector<int> ses(best_need_turn);
+            ses[0] = 1;
+            string tpos, apos;
+            // 逆順に詰める
+            Pos cpos = best_taka;
+            Pos ppos = NN[taka.index()][best_taka].prev_pos;
+            if (!(taka == best_taka)) {
+                while (1) {
+                    // ppos.print();
+                    // cout << "T " << calc_dir(ppos, cpos) << endl;
+                    // cpos.print();
+                    tpos += calc_dir(ppos, cpos);
+                    cpos = ppos;
+                    ppos = NN[taka.index()][ppos].prev_pos;
+
+                    if (ppos == Pos(-1, -1))
+                        break;
+                }
+            }
+            assert(tpos.size() <= best_need_turn);
+            while (tpos.size() < best_need_turn) {
+                tpos += '.';
+            }
+            // 逆順に詰める
+            cpos = best_aoki;
+            ppos = NN[aoki.index()][best_aoki].prev_pos;
+            if (!(aoki == best_aoki)) {
+                while (1) {
+                    // ppos.print();
+                    // cout << "A " << calc_dir(ppos, cpos) << endl;
+                    // cpos.print();
+                    apos += calc_dir(ppos, cpos);
+                    cpos = ppos;
+                    ppos = NN[aoki.index()][ppos].prev_pos;
+
+                    if (ppos == Pos(-1, -1))
+                        break;
+                }
+            }
+            assert(apos.size() <= best_need_turn);
+            while (apos.size() < best_need_turn) {
+                apos += '.';
+            }
+            // cout << tpos << endl;
+            // cout << apos << endl;
+            reverse(all(tpos));
+            reverse(all(apos));
+
+            rep(i, best_need_turn) {
+                // cout << ses[i] SP << tpos[i] SP << apos[i] << endl;
+                ans.emplace_back(ses[i], tpos[i], apos[i]);
+            }
+
+            taka = best_taka;
+            aoki = best_aoki;
+            exchange(taka, aoki, true);
+            // cout << "exchanged" << endl;
+        }
+    }
+
     void output_board() {
         outputFile << "board" << endl;
         PVV(board);
-        outputFile << "taka" << endl;
-        PVV(taka_visit);
-        outputFile << "aoki" << endl;
-        PVV(aoki_visit);
+        // outputFile << "taka" << endl;
+        // PVV(taka_visit);
+        // outputFile << "aoki" << endl;
+        // PVV(aoki_visit);
     }
     void print_ans() {
         cout << pi SP << pj SP << qi SP << qj << endl;
@@ -392,6 +517,7 @@ struct Grid {
 void inpt() {
     // cout << "inpt" << endl;
     cin >> t >> n;
+    look = ceil(n);
     ve.resize(n);
     ho.resize(n - 1);
     a.resize(n, vector<int>(n));
@@ -408,6 +534,7 @@ int main() {
     start = chrono::system_clock::now();
 
     inpt();
+    calc_all_NN(look);
 
     // Grid grid;
     // grid.init();
@@ -434,7 +561,7 @@ int main() {
 
         Grid grid = base;
         grid.set_random_start();
-        grid.random_walk_and_dfs();
+        grid.NN_walk();
         long long score = grid.getD();
 
         if (best_score > score) {
