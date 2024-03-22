c30f093c8abc4c3ccfc9cf2cc2652ca6f87ae78e
berry8192 <berryrt8192@gmail.com>
2024-03-23 02:41:06 +0900

    密度計算

--- ahc031/WDN.cc
+++ ahc031/WDN.cc
@@ -3,12 +3,27 @@
 
 using namespace std;
 
+int W, D, N, su = 0;
+int a[50][50];
+
+void inpt() {
+    cin >> W >> D >> N;
+
+    for (int i = 0; i < D; i++) {
+        for (int j = 0; j < N; j++) {
+            cin >> a[i][j];
+            su += a[i][j];
+        }
+    }
+}
+
 int main() {
     std::ofstream outputFile("log.csv", std::ios::app);
 
-    int W, D, N;
-    cin >> W >> D >> N;
-    outputFile << W << ", " << D << ", " << N << endl;
+    inpt();
+
+    outputFile << W << ", " << D << ", " << N << ", " << 1.0 * su / D / W / W
+               << endl;
 
     outputFile.close();
 
