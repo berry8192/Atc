71d4d29b0aaf72e2fb36b591fe64e0b28a3da7de
berry8192 <berryrt8192@gmail.com>
2024-09-15 20:30:06 +0900

    greed_mini_cost

--- ahc037/A.cc
+++ ahc037/A.cc
@@ -73,6 +73,7 @@ struct Pos {
     }
 
     int manhattan(Pos pos) { return abs(h - pos.h) + abs(w - pos.w); }
+    bool is_right_up_by(Pos pos) { return h >= pos.h && w >= pos.w; }
     void print() { cout << "(" << h << ", " << w << ")" << endl; }
 
     bool operator<(const Pos &in) const {
@@ -94,8 +95,77 @@ struct Pos {
         return rtn;
     }
 };
+vector<Pos> AB;
 
-struct Grid {};
+struct Line {
+    Pos from;
+    Pos to;
+
+    Line() {};
+    Line(Pos from_pos, Pos to_pos) {
+        from = from_pos;
+        to = to_pos;
+    }
+
+    void output_line() {
+        cout << from.h << " " << from.w << " " << to.h << " " << to.w << endl;
+    }
+};
+
+struct Graph {
+    vector<Line> lines;
+    vector<Line> sorted_lines;
+
+    Graph() {};
+
+    void greed_mini_cost() {
+        cout << 1000 << endl;
+        rep(i, N) {
+            int min_cost = AB[i].manhattan({0, 0});
+            int min_cost_index = N;
+
+            rep(j, N) {
+                if (i == j)
+                    continue;
+                if (!AB[i].is_right_up_by(AB[j]))
+                    continue;
+                int cost = AB[i].manhattan(AB[j]);
+                if (cost < min_cost) {
+                    min_cost = cost;
+                    min_cost_index = j;
+                }
+            }
+            lines.push_back({AB[min_cost_index], AB[i]});
+        }
+        sort_lines();
+        print_lines();
+    }
+    void sort_lines() {
+        set<Pos> connected;
+        connected.insert({0, 0});
+        vector<int> conn_lines(lines.size());
+
+        while (1) {
+            int connect_count = 0;
+            rep(i, lines.size()) {
+                if (conn_lines[i])
+                    continue;
+                if (connected.find(lines[i].from) != connected.end()) {
+                    sorted_lines.push_back(lines[i]);
+                    connected.insert(lines[i].to);
+                    conn_lines[i] = 1;
+                    connect_count++;
+                }
+            }
+            if (connect_count == 0)
+                break;
+        }
+    }
+
+    void print_lines() {
+        rep(i, sorted_lines.size()) { sorted_lines[i].output_line(); }
+    }
+};
 
 void all_zero() {
     cout << 1000 << endl;
@@ -104,15 +174,25 @@ void all_zero() {
 
 void inpt() {
     cin >> N;
-    A.resize(N);
-    B.resize(N);
-    rep(i, N) cin >> A[i] >> B[i];
+    A.resize(N + 1);
+    B.resize(N + 1);
+    AB.resize(N + 1);
+    rep(i, N) {
+        cin >> A[i] >> B[i];
+        AB[i] = Pos(A[i], B[i]);
+    }
+    A[N] = 0;
+    B[N] = 0;
+    AB[N] = Pos(0, 0);
 }
 
 int main() {
     start = chrono::system_clock::now();
 
     inpt();
+
+    Graph graph;
+    graph.greed_mini_cost();
 
     return 0;
 }
