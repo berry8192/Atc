6a3ba07c4a2f5d65c0969fdaad455447a3fbed60
berry8192 <berryrt8192@gmail.com>
2024-09-15 19:13:31 +0900

    雑回答

---
+++ ahc037/A.cc
@@ -0,0 +1,116 @@
+#include <bits/stdc++.h>
+// #include <atcoder/all>
+
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep3(i, n, m) for (int i = m; i < (int)(n); i++)
+#define repr(i, n) for (int i = n - 1; i >= 0; i--)
+#define all(v) v.begin(), v.end()
+#define SP << " "
+#define ll long long
+
+using namespace std;
+// using namespace atcoder;
+
+// std::ofstream outputFile("log.csv");
+
+// template <class T> void PV(T pvv) {
+// 	if(!pvv.size()) return;
+// 	rep(i, pvv.size()-1) outputFile << pvv[i] << ", ";
+// 	outputFile<< pvv[pvv.size()-1] <<endl;
+// }
+// template <class T> void PV(T pvv) {
+// 	if(!pvv.size()) return;
+// 	rep(i, pvv.size()-1) cout << pvv[i] << ", ";
+// 	// rep(i, pvv.size()-1) cout<< pvv[i]/20 SP << pvv[i]%20 <<endl;
+// 	cout<< pvv[pvv.size()-1] <<endl;
+// }
+
+// template <class T>void PVV(T pvv) {
+// 	rep(i, pvv.size()){
+//         // outputFile<< "i: " << i <<endl;
+// 		rep(j, pvv[i].size()-1){
+// 			outputFile<< pvv[i][j] << ", ";
+// 		}
+// 		if(pvv[i].size()) outputFile<< pvv[i][pvv[i].size()-1] <<endl;
+// 	}
+// }
+// template <class T> void PM(T pm) {
+//     // cout<< "{";
+// 	for(auto m : pm){
+// 		outputFile<< "(" << m.first << "->" << m.second << "), ";
+// 	}
+// 	// cout<< "}";
+// 	outputFile<<endl;
+// }
+
+int imax = 2147483647;
+long long llimax = 9223372036854775807;
+
+// 焼きなましの定数
+chrono::system_clock::time_point start, current;
+double TIME_LIMIT = 1900.0;
+// double TIME_LIMIT=190.0;
+double start_temp = 10000000.0;
+double end_temp = 10000.0;
+
+// 乱数の準備
+// auto seed=(unsigned)time(NULL);
+int seed = 1;
+mt19937 mt(seed);
+
+int N;
+vector<int> A, B;
+
+// 構造体
+struct Pos {
+    int h;
+    int w;
+
+    Pos() {};
+    Pos(int hh, int ww) {
+        h = hh;
+        w = ww;
+    }
+
+    int manhattan(Pos pos) { return abs(h - pos.h) + abs(w - pos.w); }
+    void print() { cout << "(" << h << ", " << w << ")" << endl; }
+
+    bool operator<(const Pos &in) const {
+        return h != in.h ? h < in.h : w < in.w;
+    };
+    bool operator==(const Pos &cpos) const {
+        return (h == cpos.h && w == cpos.w);
+    };
+    Pos operator+(const Pos pos) {
+        Pos rtn;
+        rtn.h = h + pos.h;
+        rtn.w = w + pos.w;
+        return rtn;
+    }
+    Pos operator-(const Pos pos) {
+        Pos rtn;
+        rtn.h = h - pos.h;
+        rtn.w = w - pos.w;
+        return rtn;
+    }
+};
+
+struct Grid {};
+
+void inpt() {
+    cin >> N;
+    A.resize(N);
+    B.resize(N);
+    rep(i, N) cin >> A[i] >> B[i];
+}
+
+int main() {
+    start = chrono::system_clock::now();
+
+    inpt();
+
+    cout << 1000 << endl;
+    rep(i, N) { cout << 0 SP << 0 SP << A[i] SP << B[i] << endl; }
+
+    return 0;
+}
