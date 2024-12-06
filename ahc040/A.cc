54d9fad8e1d15e055f18294b639c5dde3925ab6c
berry8192 <berryrt8192@gmail.com>
2024-12-06 23:47:03 +0900

    構造体作成

---
+++ ahc040/A.cc
@@ -0,0 +1,166 @@
+#include <bits/stdc++.h>
+// #include <atcoder/all>
+
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep3(i, n, m) for (int i = m; i < (int)(n); i++)
+#define repr(i, n) for (int i = n - 1; i >= 0; i--)
+#define all(v) v.begin(), v.end()
+#define SP << " "
+#define ll long long
+
+using namespace std;
+// using namespace atcoder;
+
+// std::ofstream outputFile("log.csv");
+
+// template <class T> void PV(T pvv) {
+// 	if(!pvv.size()) return;
+// 	rep(i, pvv.size()-1) outputFile << pvv[i] << ", ";
+// 	outputFile<< pvv[pvv.size()-1] <<endl;
+// }
+// template <class T> void PV(T pvv) {
+// 	if(!pvv.size()) return;
+// 	rep(i, pvv.size()-1) cout << pvv[i] << ", ";
+// 	// rep(i, pvv.size()-1) cout<< pvv[i]/20 SP << pvv[i]%20 <<endl;
+// 	cout<< pvv[pvv.size()-1] <<endl;
+// }
+
+// template <class T>void PVV(T pvv) {
+// 	rep(i, pvv.size()){
+//         // outputFile<< "i: " << i <<endl;
+// 		rep(j, pvv[i].size()-1){
+// 			outputFile<< pvv[i][j] << ", ";
+// 		}
+// 		if(pvv[i].size()) outputFile<< pvv[i][pvv[i].size()-1] <<endl;
+// 	}
+// }
+// template <class T> void PM(T pm) {
+//     // cout<< "{";
+// 	for(auto m : pm){
+// 		outputFile<< "(" << m.first << "->" << m.second << "), ";
+// 	}
+// 	// cout<< "}";
+// 	outputFile<<endl;
+// }
+
+int imax = 2147483647;
+long long llimax = 9223372036854775807;
+
+// 焼きなましの定数
+chrono::system_clock::time_point start, current;
+double TIME_LIMIT = 1900.0;
+// double TIME_LIMIT=190.0;
+double start_temp = 10000000.0;
+double end_temp = 10000.0;
+
+struct Timer {
+    chrono::_V2::system_clock::time_point start;
+
+    Timer() { start = chrono::system_clock::now(); }
+    double progress() {
+        chrono::_V2::system_clock::time_point current =
+            chrono::system_clock::now();
+        return chrono::duration_cast<chrono::milliseconds>(current - start)
+                   .count() /
+               TIME_LIMIT;
+    }
+};
+Timer timer;
+
+// 乱数の準備
+// auto seed=(unsigned)time(NULL);
+int seed = 1;
+mt19937 mt(seed);
+
+int N, T, sigma;
+
+// 構造体
+struct Pos {
+    int h;
+    int w;
+
+    Pos() {};
+    Pos(int hh, int ww) {
+        h = hh;
+        w = ww;
+    }
+
+    bool is_oob() {
+        // assert(0<=h);
+        // assert(h<n);
+        // assert(0<=w);
+        // assert(w<=h);
+        return !(0 <= h && 0 <= w);
+    }
+    int manhattan(Pos pos) { return abs(h - pos.h) + abs(w - pos.w); }
+    void print() { cout << "(" << h << ", " << w << ")" << endl; }
+    // 受け取った近傍でPosを作る
+    vector<Pos> around_pos(const vector<Pos> &dir) {
+        vector<Pos> rtn;
+        Pos new_pos;
+        for (int i = 0; i < dir.size(); i++) {
+            new_pos = {h + dir[i].h, w + dir[i].w};
+            if (!new_pos.is_oob())
+                rtn.emplace_back(new_pos);
+        }
+        return rtn;
+    }
+
+    bool operator<(const Pos &in) const {
+        return h != in.h ? h < in.h : w < in.w;
+    };
+    bool operator==(const Pos &cpos) const {
+        return (h == cpos.h && w == cpos.w);
+    };
+    Pos operator+(const Pos pos) {
+        Pos rtn;
+        rtn.h = h + pos.h;
+        rtn.w = w + pos.w;
+        return rtn;
+    }
+    Pos operator-(const Pos pos) {
+        Pos rtn;
+        rtn.h = h - pos.h;
+        rtn.w = w - pos.w;
+        return rtn;
+    }
+};
+
+vector<Pos> initial_rectangles;
+
+struct Rectangle {
+    Pos top_left;
+    int height;
+    int width;
+
+    Rectangle(Pos tl, int hh, int ww) {
+        top_left = tl;
+        height = hh;
+        width = ww;
+    }
+};
+
+struct Board {
+    vector<Rectangle> rectangle;
+};
+
+void inpt() {
+    cin >> N >> T >> sigma;
+    int ih, iw;
+    rep(i, N) {
+        cin >> ih >> iw;
+        initial_rectangles.emplace_back(ih, iw);
+    }
+}
+
+int main() {
+    Board board;
+
+    int loop = 0;
+    while (timer.progress() < 1) {
+        loop++;
+    }
+    // board.print_ans();
+
+    return 0;
+}
