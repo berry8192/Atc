611e8d0c0e394ebecc8b8c708cdbc2db5f996de3
berry8192 <berryrt8192@gmail.com>
2025-05-18 19:22:27 +0900

    100の回答

---
+++ ahc047/A.cc
@@ -0,0 +1,114 @@
+#include <bits/stdc++.h>
+// #include <atcoder/all>
+
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep3(i, n, m) for (int i = m; i < (int)(n); i++)
+#define repr(i, n) for (int i = n - 1; i >= 0; i--)
+#define all(v) v.begin(), v.end()
+#define SP << " "
+#define ll long long
+
+using namespace std;
+// using namespace atcoder;
+
+// std::ofstream outputFile("log.csv");
+
+// template <class T> void PV(T pvv) {
+// 	if(!pvv.size()) return;
+// 	rep(i, pvv.size()-1) outputFile << pvv[i] << ", ";
+// 	outputFile<< pvv[pvv.size()-1] <<endl;
+// }
+// template <class T> void PV(T pvv) {
+// 	if(!pvv.size()) return;
+// 	rep(i, pvv.size()-1) cout << pvv[i] << ", ";
+// 	// rep(i, pvv.size()-1) cout<< pvv[i]/20 SP << pvv[i]%20 <<endl;
+// 	cout<< pvv[pvv.size()-1] <<endl;
+// }
+
+// template <class T>void PVV(T pvv) {
+// 	rep(i, pvv.size()){
+//         // outputFile<< "i: " << i <<endl;
+// 		rep(j, pvv[i].size()-1){
+// 			outputFile<< pvv[i][j] << ", ";
+// 		}
+// 		if(pvv[i].size()) outputFile<< pvv[i][pvv[i].size()-1] <<endl;
+// 	}
+// }
+// template <class T> void PM(T pm) {
+//     // cout<< "{";
+// 	for(auto m : pm){
+// 		outputFile<< "(" << m.first << "->" << m.second << "), ";
+// 	}
+// 	// cout<< "}";
+// 	outputFile<<endl;
+// }
+
+int imax = 2147483647;
+long long llimax = 9223372036854775807;
+
+// 焼きなましの定数
+chrono::system_clock::time_point start, current;
+double TIME_LIMIT = 1900.0;
+// double TIME_LIMIT=190.0;
+double start_temp = 10000000.0;
+double end_temp = 10000.0;
+
+struct Timer {
+    chrono::_V2::system_clock::time_point start;
+
+    Timer() { start = chrono::system_clock::now(); }
+    double progress() {
+        chrono::_V2::system_clock::time_point current =
+            chrono::system_clock::now();
+        return chrono::duration_cast<chrono::milliseconds>(current - start)
+                   .count() /
+               TIME_LIMIT;
+    }
+};
+Timer timer;
+
+// 乱数の準備
+// auto seed=(unsigned)time(NULL);
+int seed = 1;
+mt19937 mt(seed);
+
+int N, M, L;
+vector<string> S;
+vector<int> P;
+
+struct Model {
+    vector<char> C;
+    vector<vector<int>> A;
+    Model() {
+        C.assign(M, 'a');
+        A.assign(M, vector<int>(M, 0));
+        for (int i = 0; i < M; i++) {
+            A[i][0] = 100;
+        }
+    }
+    void print_ans() {
+        for (int i = 0; i < M; i++) {
+            cout << C[i];
+            for (int j = 0; j < M; j++) {
+                cout << " " << A[i][j];
+            }
+            cout << endl;
+        }
+    }
+};
+
+void inpt() {
+    cin >> N >> M >> L;
+    S.resize(N);
+    P.resize(N);
+    for (int i = 0; i < N; i++) {
+        cin >> S[i] >> P[i];
+    }
+}
+
+int main() {
+    inpt();
+    Model model;
+    model.print_ans();
+    return 0;
+}
