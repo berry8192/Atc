b7abde82447177cd9c316a0c2cd152de9a8752f2
berry8192 <berryrt8192@gmail.com>
2025-12-07 00:01:24 +0900

    tmp修正

--- abctmp/A.cc
+++ abctmp/A.cc
@@ -16,9 +16,21 @@ using vvll = vector<vll>;
 using vs = vector<string>;
 using vpii = vector<pii>;
 using vpll = vector<pll>;
+using tii = tuple<int, int>;
+using tiii = tuple<int, int, int>;
+using tiiii = tuple<int, int, int, int>;
+using tll = tuple<ll, ll>;
+using tlll = tuple<ll, ll, ll>;
+using tllll = tuple<ll, ll, ll, ll>;
+using vtii = vector<tii>;
+using vtiii = vector<tiii>;
+using vtiiii = vector<tiiii>;
+using vtll = vector<tll>;
+using vtlll = vector<tlll>;
+using vtllll = vector<tllll>;
 
 // ========== 定数 ==========
-const int INF = 1e9;
+const int INF = 2e9;
 const ll LINF = 1e18;
 const int MOD = 1000000007;
 const int MOD2 = 998244353;
@@ -166,6 +178,27 @@ istream &operator>>(istream &is, pair<T, U> &p) {
     return is;
 }
 
+// tuple入力（2要素）
+template <typename T1, typename T2>
+istream &operator>>(istream &is, tuple<T1, T2> &t) {
+    is >> get<0>(t) >> get<1>(t);
+    return is;
+}
+
+// tuple入力（3要素）
+template <typename T1, typename T2, typename T3>
+istream &operator>>(istream &is, tuple<T1, T2, T3> &t) {
+    is >> get<0>(t) >> get<1>(t) >> get<2>(t);
+    return is;
+}
+
+// tuple入力（4要素）
+template <typename T1, typename T2, typename T3, typename T4>
+istream &operator>>(istream &is, tuple<T1, T2, T3, T4> &t) {
+    is >> get<0>(t) >> get<1>(t) >> get<2>(t) >> get<3>(t);
+    return is;
+}
+
 // Yes/No出力
 void Yes(bool flag = true) { cout << (flag ? "Yes" : "No") << "\n"; }
 
@@ -200,15 +233,7 @@ void solve() {
     vi a(n);
     cin >> a; // vector入力
 
-    // 例: 処理
-    ll sum = 0;
-    rep(i, n) { sum += a[i]; }
-
-    // 例: 出力
-    cout << sum << "\n";
-
-    // 例: Yes/No出力
-    Yes(sum > 100);
+    rep(i, n) {}
 
     // 例: デバッグ出力（ローカルのみ）
     debug(n, sum, a);
