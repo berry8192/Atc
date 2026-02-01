8ca9b7c3eaa2b3462652804df2b5f06e65491006
berry8192 <berryrt8192@gmail.com>
2026-02-01 19:47:19 +0900

    パス列挙

--- ahc060/A.cc
+++ ahc060/A.cc
@@ -24,6 +24,67 @@ int main() {
         cin >> x >> y;
     }
 
+    // 店から店への経路を列挙（BFSで経路長10未満のみ）
+    // paths[出発店][ゴール店] = {経路1, 経路2, ...}
+    // 各経路は頂点番号の列
+    vector<vector<vector<vector<int>>>> paths(K,
+                                              vector<vector<vector<int>>>(K));
+
+    rep(start, K) {
+        // 開始店からBFSで経路を探索
+        queue<vector<int>> q;
+        q.push({start});
+
+        while (!q.empty()) {
+            vector<int> path = q.front();
+            q.pop();
+
+            int current = path.back();
+
+            // 経路長が10以上なら打ち切り
+            if (path.size() >= 10)
+                continue;
+
+            // 隣接頂点を探索
+            for (int next : graph[current]) {
+                // 直前の頂点には戻らない（経路に2つ以上頂点がある場合）
+                if (path.size() >= 2 && next == path[path.size() - 2])
+                    continue;
+
+                vector<int> new_path = path;
+                new_path.push_back(next);
+
+                // nextが店なら経路として保存（ただし探索は続けない）
+                if (next < K && next != start) {
+                    paths[start][next].push_back(new_path);
+                    // 店に到達したらそこで探索終了（キューに入れない）
+                } else {
+                    // 木のマスなら探索を続ける
+                    q.push(new_path);
+                }
+            }
+        }
+    }
+
+    // デバッグ出力（経路数を確認）
+    cerr << "Path enumeration done:" << endl;
+    int total_paths = 0;
+    rep(i, K) {
+        int sum_from_i = 0;
+        rep(j, K) {
+            if (i != j) {
+                int cnt = paths[i][j].size();
+                cerr << "Shop " << i << " -> Shop " << j << ": " << cnt
+                     << " paths" << endl;
+                sum_from_i += cnt;
+            }
+        }
+        cerr << "From Shop " << i << " total: " << sum_from_i << " paths"
+             << endl;
+        total_paths += sum_from_i;
+    }
+    cerr << "Total paths: " << total_paths << endl;
+
     // 乱数の準備
     mt19937 mt(12345);
 
